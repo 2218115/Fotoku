49e73ba62a20381cd06cd13dc48acc191814ff83
DESKTOP-P04P4QG\makrusali <rindikid361@gmail.com>
2025-05-03 11:23:46 +0700

    [BAB 5] NAVIGATION

--- src/assets/colors.jsx
+++ src/assets/colors.jsx
@@ -2,7 +2,7 @@ export default {
   primary: '#111111',
   primaryLight: '#f1f1f1',
   secondary: '#6f6f6f',
-  accent: '#007AFF',
+  accent: '#007Aaa',
   border: '#f5f5f5',
   background: '#FFFFFF',
 };
